b8a27771f613dc262ca14423290212676fd1bff9
Samuel Ieremie <ieremie.samuel@gmail.com>
2026-03-31 20:17:19 +0200

    Fix import Timer component bug
    The component was deleted and the import wasn't removed, leading to a weird bug.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import "./App.css";
 import MineSweeper from "./components/MineSweeper";
-import Timer from "./components/Timer";
 
 function App() {
   return <MineSweeper difficulty={0} />;
